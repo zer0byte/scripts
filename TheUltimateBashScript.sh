245f610c27de5a13bc8cd46a90c06312ee8d72a2
zer0byte <thezerobytemail@gmail.com>
2019-02-10 20:31:38 -0500

    Update TheUltimateBashScript.sh

--- TheUltimateBashScript.sh
+++ TheUltimateBashScript.sh
@@ -3,12 +3,13 @@ apt-get install ufw
 ufw allow 22
 ufw enable
 apt-get install nmap masscan geoip-bin sshuttle git python-pip libssl-dev libffi-dev python-dev build-essential -y
+apt-get install curl git libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 auto$
 mkdir /Tools
 ls
 cd /Tools
 wget https://git.io/vpn -O openvpn-install.sh
 git clone https://github.com/trustedsec/ptf.git
-apt-get install curl git libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 auto$
+git clone https://github.com/davidtavarez/pwndb
 echo "deb https://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/ruby.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C3173AA6
 apt-get install ruby2.3 ruby2.3-dev bundler -y
