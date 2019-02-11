ee053e822201678064c6cc87d0443f1fd479d6b4
zer0byte <thezerobytemail@gmail.com>
2019-02-10 20:18:01 -0500

    Create APT -01 - Internal AD Recon.bat

---
+++ APT -01 - Internal AD Recon.bat
@@ -0,0 +1,17 @@
+@echo off
+ipconfig /all>>1.txt
+net start>>1.txt
+tasklist /v>>1.txt
+net user >>1.txt
+net localgroup administrators>>1.txt
+netstat -ano>>1.txt
+net use>>1.txt
+net view>>1.txt
+net view /domain>>1.txt
+net group /domain>>1.txt
+net group “domain users” /domain>>1.txt
+net group “domain admins” /domain>>1.txt
+net group “domain controllers” /domain>>1.txt
+net group “exchange domain servers” /domain>>1.txt
+net group “exchange servers” /domain>>1.txt
+net group “domain computers” /domain>>”1.txt
