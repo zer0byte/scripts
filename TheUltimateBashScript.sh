2566176e8403c946fd15adf70cb9911806855b02
zer0byte <thezerobytemail@gmail.com>
2019-07-04 19:42:05 -0400

    Update TheUltimateBashScript.sh

--- TheUltimateBashScript.sh
+++ TheUltimateBashScript.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
+echo "[+] Starting Install [+]"
+echo "[+] Upgrade installed packages to latest [+]"
 apt-get update && apt-get upgrade
+apt-get autoclean
+
+echo "[+] Installing Uncomplicated Firewall [+]"
 apt-get install ufw
+
+echo "[+] Configuring Uncomplicated Firewall [+]"
 ufw allow 22
 ufw enable
+
 apt-get install nmap masscan geoip-bin sshuttle git python-pip libssl-dev libffi-dev python-dev build-essential -y
 apt-get install curl git libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 auto$
 apt-get install python-requests
