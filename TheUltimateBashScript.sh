45b443ff9527a67df5d9621a019eb848b1594c60
zer0byte <thezerobytemail@gmail.com>
2020-02-08 22:51:44 -0500

    Update TheUltimateBashScript.sh

--- TheUltimateBashScript.sh
+++ TheUltimateBashScript.sh
@@ -17,10 +17,9 @@ echo " "
 ufw enable
 echo " "
 echo " "
-apt-get install nmap masscan geoip-bin sshuttle git python-pip libssl-dev libffi-dev python-dev build-essential -y
-apt-get install curl git libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 auto$
+apt-get install nmap masscan net-tools geoip-bin sshuttle git python-pip libssl-dev libffi-dev python-dev build-essential -y
+apt-get install curl git locate libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 auto$
 apt-get install python-requests
-apt-get install net-tools
 echo " "
 echo " "
 mkdir Tools
