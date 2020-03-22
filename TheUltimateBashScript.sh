3467ed0c2cb2bcb93a3fa083d183c0dfa0d70188
zer0byte <thezerobytemail@gmail.com>
2020-03-22 18:10:51 -0400

    Update TheUltimateBashScript.sh

--- TheUltimateBashScript.sh
+++ TheUltimateBashScript.sh
@@ -29,28 +29,68 @@ echo " "
 cd Tools
 echo " "
 echo " "
-echo "installing dirsearch"
+echo "[+] Installing dirsearch [+]"
 echo " "
 echo " "
 git clone https://github.com/maurosoria/dirsearch.git
 echo " "
 echo " "
 wget https://git.io/vpn -O openvpn-install.sh
+echo " "
+echo " "
+echo "[+] Installing Pentester Framework [+]"
+echo " "
+echo " "
 git clone https://github.com/trustedsec/ptf.git
+echo " "
+echo " "
+echo "[+] Installing Sublist3r [+]"
+echo " "
+echo " "
 git clone https://github.com/aboul3la/Sublist3r.git
+echo " "
+echo " "
+echo "[+] Installing Responder [+]"
+echo " "
+echo " "
 git clone https://github.com/SpiderLabs/Responder.git
+echo " "
+echo " "
+echo "[+] Installing Nikto [+]"
+echo " "
+echo " "
 git clone https://github.com/sullo/nikto.git
+echo " "
+echo " "
+echo "[+] Installing PwnDB [+]"
+echo " "
+echo " "
 git clone https://github.com/davidtavarez/pwndb
 apt install apt-transport-https
 apt install apt-transport-tor
+cd /root/Tools/pwndb
+pip install -r requirements.txt
+echo " "
+echo " "
+echo "[+] Installing WPScan [+]"
+echo " "
+echo " "
 echo "deb https://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/ruby.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C3173AA6
 apt-get install ruby2.3 ruby2.3-dev bundler -y
 git clone https://github.com/wpscanteam/wpscan.git
 cd wpscan
 bundle install --without test
-alias wpscan='/root/Tools/wpscan/./wpscan.rb' 
+alias wpscan='/root/Tools/wpscan/./wpscan.rb'
+echo " "
+echo " "
+echo "[+] Installing Crackmapexc [+]"
+echo " "
+echo " "
 pip install crackmapexec
+echo " "
+echo " "
+echo "[+] Installing WebScreenshot [+]"
+echo " "
+echo " "
 pip install webscreenshot
-cd /root/Tools/pwndb
-pip install -r requirements.txt
